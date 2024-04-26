664008deedc1f534b34c362fe05460391eed08d4
Timur <trytomakeyourselfbetter@gmail.com>
2024-04-26 15:22:21 +0500

    add files

---
+++ script.js
@@ -0,0 +1,38 @@
+
+// // Первый таск
+
+// let firstName = prompt('Введите Ваше имя');
+// let age = prompt('Введите Ваш возраст');
+// let exercise = +prompt('Решите пример 998 + 1026 = ?');
+// let exercise2 = +prompt('Решите пример 100 - 200 = ?');
+// let exercise3 = +prompt('Решите пример 10 * 2200 = ?');
+// let exercise4 = +prompt('Решите пример 20 / 4 = ?');
+// let exercise5 = +prompt('Решите пример 100 % 30 = ?');
+// let openConsole = ('Откройте консоль для просмотра');
+// alert(openConsole);
+// let example1 = ('Пример 1: 998 + 1026 = 2024');
+// let example2 = ('Пример 2: 100 - 200 = -100');
+// let example3 = ('Пример 3: 10 * 2200 = 22000');
+// let example4 = ('Пример 4: 20 / 4 = 5');
+// let example5 = ('Пример 5: 100 % 30 = 33');
+// let yourAnswer = ('Ваш ответ');
+
+
+// console.log('Меня зовут' + ' ' + firstName);
+// console.log('Мне' + ' ' + age);
+// console.log(example1 + ' '  + ' ' + yourAnswer + ' ' + exercise );
+// console.log(example2 + ' '  + ' ' + yourAnswer + ' ' + exercise2);
+// console.log(example3 + ' '  + ' ' + yourAnswer + ' ' + exercise3);
+// console.log(example4 + ' '  + ' ' + yourAnswer + ' ' + exercise4);
+// console.log(example5 + ' '  + ' ' + yourAnswer + ' ' + exercise5);
+
+// Второй таск
+
+// var A, S, D;
+
+// A = +prompt('Введите первое число')
+// S = +prompt('Введите второе число')
+// D = +prompt('Введите третье число')
+// let result = ((A + S + D)/3);
+
+// console.log(result);
